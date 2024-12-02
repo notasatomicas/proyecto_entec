78a73ad75a08572d5276763d28b946aea92c9269
notasatomicas <notasatomicas@gmail.com>
2024-12-02 20:34:22 -0300

    Actualizadas las rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
+$routes->get('/inicio', 'Home::index');
+$routes->get('/index', 'Home::index');
+$routes->get('/about', 'About::index');
