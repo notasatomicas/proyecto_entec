88137753f7405cf64f9f22f437804e09e23bd067
notasatomicas <notasatomicas@gmail.com>
2024-12-02 19:30:11 -0300

    Añadido el archivo de vista del footer

---
+++ app/Views/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="environment">
+
+    <p>Page rendered in {elapsed_time} seconds using {memory_usage} MB of memory.</p>
+
+    <p>Environment: <?= ENVIRONMENT ?></p>
+
+    </div>
+
+    <div class="copyrights">
+
+    <p>
+        &copy; <?= date('Y') ?> Proyecto ENTEC
+    </p>
+
+    </div>
+</footer>
