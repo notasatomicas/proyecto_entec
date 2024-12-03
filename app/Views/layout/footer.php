678b5aa147aec894e64bd6bc86d801d52583fdb0
notasatomicas <notasatomicas@gmail.com>
2024-12-03 18:08:11 -0300

    Minor fixes en las rutas

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,10 +1,10 @@
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+      <li class="nav-item"><a href="./inicio" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+      <li class="nav-item"><a href="./catalogo" class="nav-link px-2 text-body-secondary">Catalogo</a></li>
+      <li class="nav-item"><a href="./consultas" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li class="nav-item"><a href="./contacto" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+      <li class="nav-item"><a href="./about" class="nav-link px-2 text-body-secondary">Nosotros</a></li>
     </ul>
     
     <div class="environment">
