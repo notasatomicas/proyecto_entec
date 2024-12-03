941b93578c552ddf12830a45c442980637f70348
notasatomicas <notasatomicas@gmail.com>
2024-12-03 17:54:42 -0300

    Añadidas credenciales de la base de datos

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'root',
+        'password'     => 'toor',
+        'database'     => 'entec',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
