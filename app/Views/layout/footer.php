70e139aecaa02840897c44d1bcda91ea77a4cfbc
notasatomicas <notasatomicas@gmail.com>
2024-12-07 01:13:39 -0300

    Añadido nuevo footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -1,4 +1,4 @@
-<footer class="py-3 my-4">
+<!--footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
       <li class="nav-item"><a href="./inicio" class="nav-link px-2 text-body-secondary">Inicio</a></li>
       <li class="nav-item"><a href="./catalogo" class="nav-link px-2 text-body-secondary">Catalogo</a></li>
@@ -11,6 +11,33 @@
             <p class="text-center">La página fue renderizada en {elapsed_time} segundos usando {memory_usage} MB de memoria.</p>
     </div>
     <p class="text-center text-body-secondary">&copy; <?= date('Y') ?> Proyecto ENTEC</p>
-</footer>
+</footer-->
 
-</body>
+<footer class="py-3 my-4 bg-secondary-subtle">
+      <ul class="nav justify-content-center mb-1">
+            <li class="nav-item"><a href="./" class="nav-link px-2 fw-bold text-dark">Principal</a></li>
+            <li class="nav-item"><a href="./comercializacion" class="nav-link px-2 fw-bold text-dark">Comercializacion</a></li>
+            <li class="nav-item"><a href="./about" class="nav-link px-2 fw-bold text-dark">Quienes Somos</a></li>
+            <li class="nav-item"><a href="./contacto" class="nav-link px-2 fw-bold text-dark">Contacto</a></li>
+            <li class="nav-item"><a href="./terminos" class="nav-link px-2 fw-bold text-dark">Terminos y condiciones</a></li>
+      </ul>
+      <ul class="nav justify-content-center pb-3">
+            <li class="nav-item"><a onclick="window.open(this.href, '_blank', 'width=600,height=400'); return false;"
+                        href="error" class="nav-link mx-2 fw-bold text-dark">
+                        <i class="bi bi-instagram fa-beat"></i></a></li>
+            <li class="nav-item"><a onclick="window.open(this.href, '_blank', 'width=600,height=400'); return false;"
+                        href="error" class="nav-link mx-2 fw-bold text-dark">
+                        <i class="bi bi-facebook fa-beat"></i></a></li>
+            <li class="nav-item"><a onclick="window.open(this.href, '_blank', 'width=600,height=400'); return false;"
+                        href="error" class="nav-link mx-2 fw-bold text-dark">
+                        <i class="bi bi-youtube fa-beat"></i></a></li>
+            <li class="nav-item"><a onclick="window.open(this.href, '_blank', 'width=600,height=400'); return false;"
+                        href="error" class="nav-link mx-2 fw-bold text-dark">
+                        <i class="bi bi-whatsapp fa-beat"></i></a></li>
+            <li class="nav-item"><a onclick="window.open(this.href, '_blank', 'width=600,height=400'); return false;"
+                        href="error" class="nav-link mx-2 fw-bold text-dark">
+                        <i class="bi bi-github fa-beat"></i></a></li>
+      </ul>
+      <p class="text-center"><img class="me-2" src="public/assets/img/png/favicon.png" alt="" height="20" width="20"><a
+                  href="./" class="text-decoration-none text-dark fw-bold">LibreriaX - <span><script>document.write(new Date().getFullYear());</script></span></a></p>
+</footer>
