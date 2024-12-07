a74a85d67a6371898833bc7334bcf6ce8b97a5a5
notasatomicas <notasatomicas@gmail.com>
2024-12-07 01:35:46 -0300

    Minor changes en la cabecera de la página

--- app/Views/layout/head.php
+++ app/Views/layout/head.php
@@ -6,4 +6,17 @@
     <title><?= esc($page_name); ?></title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="public/assets/css/animate.min.css">
+    <link rel="stylesheet" href="public/assets/css/main.css">
+
+    <!-- ICONOS DE BOOTSTRAP -->
+    <link rel="stylesheet" href="public/assets/bootstrap-icons/font/bootstrap-icons.min.css">
+    
+    <link rel="stylesheet" href="public/assets/css/hoverme.css">
+
+    <link rel="stylesheet" href="public/assets/leaflet/leaflet.css">
+    
+    <!-- mapa -->
+    <script src="public/assets/leaflet/leaflet.js"></script>
 </head>
